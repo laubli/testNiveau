520090ba6f712f73433f43231988a17de60f95f1
E21A359N <laureline.blivet@etu.univ-nantes.fr>
2022-10-21 12:23:49 +0200

    resolution bug bdd

--- src/App.js
+++ src/App.js
@@ -32,15 +32,6 @@ function LocationMarker() {
   )
 }
 
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://admin:<password>@cluster0.o5yjy2p.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-
 function App() {
   return (
     <div className="App">
