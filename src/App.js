ee5d4b0e263156b5d44673fd9006a789bb4e5d6f
E21A359N <laureline.blivet@etu.univ-nantes.fr>
2022-10-21 12:18:49 +0200

    ajout de la map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
+import 'leaflet/dist/leaflet.css';
+import { useState } from 'react';
+import {  MapContainer,  TileLayer,  useMapEvents, Marker, Popup} from 'react-leaflet';
+import L from 'leaflet'
+
+var iconBar = L.icon({
+  iconUrl: './img/icons/bar-pin.png',
+  iconRetinaUrl: require('./img/icons/bar-pin.png'),
+  iconSize:     [30, 30], // size of the icon
+  iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+  popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+function LocationMarker() {
+  const [position, setPosition] = useState(null)
+  const map = useMapEvents({
+    click() {
+      map.locate()
+    },
+    locationfound(e) {
+      setPosition(e.latlng)
+      map.flyTo(e.latlng, map.getZoom())
+    },
+  })
+
+  return position === null ? null : (
+    <Marker position={position}>
+      <Popup>You are here</Popup>
+    </Marker>
+  )
+}
+
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://admin:<password>@cluster0.o5yjy2p.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+        <MapContainer center={[48.1140097,-1.6816562]} zoom={15} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[48.1142996,-1.6833788]} icon={iconBar}>
+            <Popup>
+              La bonne nouvelle
+            </Popup>
+          </Marker>
+          <Marker position={[48.112996,-1.6869107]} icon={iconBar}>
+            <Popup>
+              Delirium café
+            </Popup>
+          </Marker>
+          <LocationMarker />
+        </MapContainer>
+      </body>
     </div>
   );
 }
+
 
 export default App;
